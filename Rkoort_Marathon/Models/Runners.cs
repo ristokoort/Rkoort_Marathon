87f8158ce89fbc76fdd9f7cb43147828567ca108
ristokoort <114060658+ristokoort@users.noreply.github.com>
2023-03-26 17:20:26 +0300

    Add  Runners Model

---
+++ Rkoort_Marathon/Models/Runners.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Rkoort_Marathon.Models
+{
+    public class Runners
+    {
+        [Key]
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int Breaks { get; set; }
+
+        [DataType(DataType.Time)]
+        public DateTime? StartTime { get; set; }
+        [DataType(DataType.Time)]
+        public DateTime? EndTime { get; set; }
+    }
+}
