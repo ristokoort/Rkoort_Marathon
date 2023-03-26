679b70f5c8f76b2b6fb125705dd4893ab2a57a74
ristokoort <114060658+ristokoort@users.noreply.github.com>
2023-03-26 17:21:19 +0300

    Add RunnersMaster model

---
+++ Rkoort_Marathon/Models/RunnersMaster.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rkoort_Marathon.Models
+{
+    public class RunnersMaster
+    {
+        [Key]
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
