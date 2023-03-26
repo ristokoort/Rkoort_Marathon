7a2789dee5e03591330eb7af5c58b2f86acd1a42
ristokoort <114060658+ristokoort@users.noreply.github.com>
2023-03-26 18:06:17 +0300

    Add Award model

---
+++ Rkoort_Marathon/Models/Award.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Rkoort_Marathon.Models
+{
+    public class Award
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [DataType(DataType.Time)]
+        public DateTime? StartTime { get; set; }
+
+        [DataType(DataType.Time)]
+        public DateTime? EndTime { get; set; }
+        public double time { get; set; }
+    }
+}
